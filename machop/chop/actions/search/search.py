07843aef4f2dcc0b73a3cc20a6e5f707a5b7227b
Derek Lai <medereklai@gmail.com>
2024-01-30 14:19:25 +0000

    modifications to get search working

--- machop/chop/actions/search/search.py
+++ machop/chop/actions/search/search.py
@@ -69,6 +69,8 @@ def search(
         model = load_model(load_name=load_name, load_type=load_type, model=model)
         logger.info(f"Loaded model from {load_name}.")
 
+    model.to(device=torch.device("cpu"))
+
     # set up data module
     data_module.prepare_data()
     data_module.setup()
@@ -80,7 +82,7 @@ def search(
         model=model,
         model_info=model_info,
         config=search_space_config,
-        dummy_input=get_dummy_input(model_info, data_module, task, device=device),
+        dummy_input=get_dummy_input(model_info, data_module, task, device=torch.device("cpu")),
         accelerator=accelerator,
     )
     search_space.build_search_space()
